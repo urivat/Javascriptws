1cfc8d4f89c77cf79cd11cff8463ba150211274b
rashad porter <rashad.porter18@gmail.com>
2022-03-25 16:22:52 -0400

    finished more of the code

--- worksheet.js
+++ worksheet.js
@@ -1,5 +1,23 @@
 "scrict";
 
-let dayOfTheWeek = "monday"
+/*let dayOfTheWeek = "monday"
 console.log(dayOfTheWeek)
-let dayOfTheWeek = "friday"
+dayOfTheWeek = "friday"
+console.log(`I can't wait for ${dayOfTheWeek}`);*/
+
+/*2 
+let animalInput = prompt("What is your favorite animal")
+let colorInput = prompt("What is your favorite color")
+console.log(`I've never seen a ${colorInput} ${animalInput}!`)*/
+
+/*3 breakfast:sausage lunch: steak Dinner: eggs */
+let timeOfDay = 1700;
+let mealChoosen = ''
+if (timeOfDay < 1200){
+    mealChoosen ='sausage'
+}else if (timeOfDay >= 1200 && timeOfDay <=1700){
+    mealChoosen = 'steak '
+}else if (timeOfDay > 1700){
+    mealChoosen = 'eggs'
+}
+console.log(mealChoosen)
